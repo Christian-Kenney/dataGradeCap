03f93fa3c76938ef72678983fe8cf9db9acbe17d
Christian-Kenney <christiankenney@ymail.com>
2024-04-21 17:11:01 -0500

    prepare for Heroku deployment

--- dataGrade/src/main/java/com/jcmm/capStrong/capStrong.java
+++ dataGrade/src/main/java/com/jcmm/capStrong/capStrong.java
@@ -2,11 +2,33 @@ package com.jcmm.capStrong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.net.URISyntaxException;
+import java.net.URI;
 
 @SpringBootApplication
 public class capStrong {
 
 	public static void main(String[] args) {
 		SpringApplication.run(capStrong.class, args);
+	}
+
+	@Bean
+	public DataSource dataSource() throws URISyntaxException {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+		dataSource.setUrl(dbUrl);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+
+		return dataSource;
 	}
 }  	
