becd3c8ed249b915ebf24dbcf1a31287422a367b
Christian-Kenney <christiankenney@ymail.com>
2024-04-21 16:08:26 -0500

    working app for heroku

---
+++ dataGrade/src/main/java/com/jcmm/capStrong/capStrong.java
@@ -0,0 +1,12 @@
+package com.jcmm.capStrong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class capStrong {
+
+	public static void main(String[] args) {
+		SpringApplication.run(capStrong.class, args);
+	}
+}  	
